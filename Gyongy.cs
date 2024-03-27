59d4a627065ce839c26217f3a3a41e85ae241a96
stigibalint <72031994+stigibalint@users.noreply.github.com>
2024-03-27 09:50:02 +0100

    Add files via upload

---
+++ Gyongy.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfHelixToolkit
+{
+    internal class Gyongy
+    {
+
+        int x, y, z, e;
+        public Gyongy(string CSVsor)
+        {
+            string[] gyongyok = CSVsor.Split(';');
+            this.X = Convert.ToInt32(gyongyok[0]);
+            this.Y = Convert.ToInt32(gyongyok[1]);
+            this.Z = Convert.ToInt32(gyongyok[2]);
+            this.E = Convert.ToInt32(gyongyok[3]);
+        }
+
+        public int X { get => x; set => x = value; }
+        public int Y { get => y; set => y = value; }
+        public int Z { get => z; set => z = value; }
+        public int E { get => e; set => e = value; }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Media.Media3D;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using HelixToolkit.Wpf;
+
+namespace WpfHelixToolkit
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Gyongy> gyongyok = File.ReadAllLines("gyongyok.txt").Skip(1).Select(x => new Gyongy(x)).ToList();
+  
+        public MainWindow()
+        {
+            InitializeComponent();
+      
+            Random rnd = new Random();
+            int randomSzam;
+            foreach (var item in gyongyok)
+            {
+
+                     randomSzam = rnd.Next(-10, 15);
+
+                
+                EllipsoidVisual3D ellipsoidVisual3D = new EllipsoidVisual3D();
+
+                ellipsoidVisual3D.RadiusX =3;
+                ellipsoidVisual3D.RadiusY =3;
+                ellipsoidVisual3D.RadiusZ = 3;
+                ellipsoidVisual3D.Center = new Point3D(item.X * 2, item.Y * 2, (item.Z + randomSzam)*2);
+                ellipsoidVisual3D.Fill = new SolidColorBrush(Color.FromRgb((byte)rnd.Next(0,255), (byte)rnd.Next(0, 255),(byte)rnd.Next(0, 255)));
+                ter.Children.Add(ellipsoidVisual3D);
+            }
+
+
+
+
+        }
+    }
+}
